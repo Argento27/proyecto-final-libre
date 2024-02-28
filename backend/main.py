a7ca72ff5af93f22e6b326ad6ee00c3ead17ff00
unknown <eliguevara36@gmail.com>
2024-02-28 19:24:26 -0300

    agrego GET y PUT

--- backend/main.py
+++ backend/main.py
@@ -7,38 +7,34 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 
-    
-
 """ Configurar base de datos """
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'eli'
+app.config['MYSQL_PASSWORD'] ='Bnim2020'
+app.config['MYSQL_DB'] = 'bd_final_pi'
 
-
+mysql = MySQL(app)
 
 """ Definir las rutas solicitadas """
 
+@app.route('/products')
+def get_persons():
+ cur = mysql.connection.cursor()
+ cur.execute('SELECT * FROM products')
+ data = cur.fetchall()
+ print(data)
+ return jsonify({"products":data})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/products/<int:id>', methods = ['PUT'])
+def delete_person(id):
+ name = request.get_json["name"]
+ description = request.get_json["description"]
+ price = request.get_json["price"]
+ image = request.get_json["image"]
+ cur = mysql.connection.cursor()
+ cur.execute('UPDATE products SET name = %s , description = %s , price = %s , image = %s WHERE id = %s', (name,description,price,image, id))
+ mysql.connection.commit()
+ return jsonify (request.get_json())
 
 
 
